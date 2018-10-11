c09038cee58638fe489ece7da0c597f6b52bdc3d
YNWA <joe@lioneventsgroup.com>
2018-10-10 20:58:39 -0700

    working youtube API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 var searchParam = [];
 var ingredientsCounter = -1;
 var searchResponse = null;
+var searchYoutube = null;
+
+
 
 function recipeSearch(x, y) {
 var queryURL = "http://api.edamam.com/search?q=" + x + "&app_id=f2e7d5eb&app_key=f6c831dedf07d960068e68c5e0623e97";
@@ -29,7 +32,11 @@ var queryURL = "http://api.edamam.com/search?q=" + x + "&app_id=f2e7d5eb&app_key
         // View recipe button
         var viewRecipe = $("<button>");
         viewRecipe.addClass("viewRecipe");
+        viewRecipe.attr("value", searchResponse.hits[i].recipe.label);
         viewRecipe.text("View Recipe");
+        // console.log(mealTitle);
+        // console.log("Value:" + viewRecipe);
+
         // Recipe Image
         var imgTag = $("<img>");
         imgTag.addClass("recipeImage");
@@ -38,10 +45,25 @@ var queryURL = "http://api.edamam.com/search?q=" + x + "&app_id=f2e7d5eb&app_key
         
         eachResult.append(imgTag, mealTitle, viewRecipe);
         recipeDiv.prepend(eachResult);
-    }
 
+        
+        
+    }
+    searchYoutube = $(".viewRecipe").val();
 });
 }
+
+
+$(document).on("click", ".viewRecipe", function(event) {
+
+    event.preventDefault();
+    var storeTitle = $(this).val();
+    console.log(storeTitle);
+
+    youtubeSearchAPI(storeTitle);
+});
+
+
 
 // Add item
 $("#addItem").on("click", function() {
@@ -65,7 +87,6 @@ $("#recipeSearch").on("click", function() {
     $(".ingredients").val(recipeSearch(searchParam));
     $("#ingredientsList").html("");
 
-
 });
 
 
@@ -78,14 +99,26 @@ $("#recipeSearch").on("click", function() {
 
     // =================================YOUTUBE API ======================================
 
-    // var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + recipe + "&key=AIzaSyBRPCZsyEmsspOCYbRltXGrLgf8-o9YIRY";
+    
 
-    // // Creating an AJAX call for the specific recipe button being clicked
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    //     }).then(function(response) {
-    //     console.log(response);
-    //     }, function(err){
-    //     console.log('*****',err)
-    // });
+    // Creating an AJAX call for the specific recipe button being clicked
+    function youtubeSearchAPI(recipe) {
+
+
+    var queryURL2 = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=" + recipe + " recipe" + "&key=AIzaSyBRPCZsyEmsspOCYbRltXGrLgf8-o9YIRY";
+
+
+    $.ajax({
+        url: queryURL2,
+        method: "GET"
+        }).then(function(response) {
+            console.log(response);
+
+        }, function(err){
+        console.log('*****',err)
+    });
+
+    
+};
+
+
